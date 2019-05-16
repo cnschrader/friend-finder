13c279806b12ce79cdb80028ca9d7388e2642006
Caitlin Schrader <caitlinschrader@caitlins-mbp.home>
2019-05-15 20:25:38 -0500

    working server

---
+++ app/server.js
@@ -0,0 +1,23 @@
+var express = require("express");
+var path = require("path");
+
+var friendsData = require("app/data/friends.js");
+
+
+// Sets up the Express App
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+
+
+
+// listening to the port
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+  
