f09cc327b792f8c4ef675ed11b80dba5be8ba665
Caitlin Schrader <caitlinschrader@caitlins-mbp.home>
2019-05-17 19:49:07 -0500

    working get request

--- app/server.js
+++ app/server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var path = require("path");
 
 
-var friendsData = require("./app/data/friends.js");
+// var friendsData = require("./app/data/friends.js");
 
 // Sets up the Express App
 var app = express();
@@ -14,7 +14,11 @@ app.use(express.json());
 
 // GET requests
 app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "./app/public/home.html"));
+    res.sendFile(path.join(__dirname, "./public/home.html"));
+  });
+
+  app.get("/survey", function(req, res){
+    res.sendFile(path.join(__dirname, "./public/survey.html"))
   });
   
 
